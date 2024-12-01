ea53ed56307ccf3f5350aba433da2c70481d45ba
Emircan <emircandv787@gmail.com>
2024-12-01 21:39:02 +0300

    The angular ssr example has been added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"profile",
+        component:ProfileComponent
+    },
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"products",
+        component:ProductsComponent
+    }
+];
