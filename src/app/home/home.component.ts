274462667110e6f940b4b02faf84dfbb6941e18b
Emircan <emircandv787@gmail.com>
2024-12-01 22:34:51 +0300

    The afterNextRender method was tried.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { afterNextRender, AfterRenderOptions, AfterViewInit, Component, ElementRef, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  @ViewChild("defaultComponentParagraph", {
+    read: ElementRef,
+    static: true
+  }) contentRef!: ElementRef;
+
+
+
+  constructor(private renderer: Renderer2) {
+    afterNextRender(() => {
+      console.log("afternextrender method was executed.")
+      console.log(window.screen.height);
+      console.log(window.screen.width);
+     
+    });
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit was executed");
+    this.renderer.setStyle(this.contentRef.nativeElement, "color", "red");
+    // console.log(window.screen.height);
+    // console.log(window.screen.width);
+    //errors because window is  a client api.
+  }
 
 }
